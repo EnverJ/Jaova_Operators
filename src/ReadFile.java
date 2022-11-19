a92b5578098137bc1c8fbe09cdc049409b4e2eed
EnverJ <Enlem1987@gmail.com>
2022-11-19 11:16:12 -0700

    file path

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class ReadFile {
     public static void main(String[] args) {
+   ReadMe();
+   PathMe();
+
+    }
+
+    static void ReadMe(){
         File fl=new File("power.txt");
         Scanner sc= null;
         try {
@@ -16,6 +22,14 @@ public class ReadFile {
             throw new RuntimeException(e);
         }
 
+    }
 
+    static void PathMe(){
+        File pa=new File("power.txt");
+        if(pa.exists()){
+            System.out.println(pa.getName());
+            System.out.println(pa.getPath());
+            System.out.println(pa.getAbsoluteFile());
+        }
     }
 }
