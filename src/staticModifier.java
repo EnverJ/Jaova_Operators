777c36ba6f6c678324dc1bddb329f342c3bc561f
EnverJ <Enlem1987@gmail.com>
2022-11-20 16:50:02 -0700

    modifier

---
+++ src/staticModifier.java
@@ -0,0 +1,12 @@
+public class staticModifier {
+
+    static int s=23;
+    static int v=12;
+
+    public static void main(String[] args) {
+
+        staticModifier sm=new staticModifier();
+        s
+
+    }
+}
