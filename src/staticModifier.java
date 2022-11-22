f4dbe234653dd24cc008e20118c1f3f3813d1cd3
EnverJ <Enlem1987@gmail.com>
2022-11-21 21:00:26 -0700

    abstract

--- src/staticModifier.java
+++ src/staticModifier.java
@@ -2,11 +2,20 @@ public class staticModifier {
 
     static int s=23;
     static int v=12;
+    static  void myStatic(){
+        System.out.println("This is static method");
+    }
+
+    public void nonStatic(){
+        System.out.println("this is non-static method");
+    }
 
     public static void main(String[] args) {
 
         staticModifier sm=new staticModifier();
-        s
+        myStatic();
+        sm.nonStatic();
+        System.out.println(s+v);
 
     }
 }
