394e76d30b2ca990469da88c29dbd210b584d39f
EnverJ <Enlem1987@gmail.com>
2022-11-22 21:55:23 -0700

    constructor

---
+++ src/ConstructorMain.java
@@ -0,0 +1,13 @@
+public class ConstructorMain {
+
+    int x;
+    public  ConstructorMain(){
+        x=5;
+
+    }
+
+    public static void main(String[] args) {
+        ConstructorMain main=new ConstructorMain();
+        System.out.println(main.x);
+    }
+}
